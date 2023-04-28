8d20004d3915d7d348d2702c9f65747d7d5422b6
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-28 21:15:17 +0530

    Added:Create simple linked list and adding data

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -24,6 +24,22 @@ public class LinkedList {
         head = new_node;
     }
 
+    // Driver code
+    public static void main(String[] args) {
+        LinkedList llist = new LinkedList();
+
+        // Create node with data 56
+        llist.push(56);
+
+        // Add node with data 30 after node with data 56
+        llist.head.next = new Node(30);
+
+        // Add node with data 70 after node with data 30
+        llist.head.next.next = new Node(70);
+
+        System.out.println("Linked list:");
+        llist.printList();
+    }
     // Method to print the linked list
     public void printList() {
         Node tnode = head;
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -1,12 +1,19 @@
 package com.bridgelabz;
 
+import org.w3c.dom.*;
+
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList llist = new LinkedList();
 
-        llist.push(70);
-        llist.push(30);
+        // Create node with data 56
         llist.push(56);
+
+        // Add node with data 30 after node with data 56
+        llist.head.next = new LinkedList.Node(30);
+
+        // Add node with data 70 after node with data 30
+        llist.head.next.next = new LinkedList.Node(70);
 
         System.out.println("Linked list:");
         llist.printList();
