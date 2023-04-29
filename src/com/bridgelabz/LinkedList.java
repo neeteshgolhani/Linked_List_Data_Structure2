b882af3b474b4968fdc46c822ded976504f38fe9
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-29 15:52:49 +0530

    Added : Delete first element In Linked List

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,14 +1,13 @@
 package com.bridgelabz;
-/*The LinkedList class has a new method, insertAfter, which inserts a new node with
-the given data after the given node. If the given node is null, it prints an error
-message and does nothing. Otherwise, it creates a new node with the given data, sets its next
-pointer to the next node of the given node,
-and sets the next pointer of the given node to the new node.
+/*The LinkedList class has a new method, pop, which deletes the first element in the list
+and updates the head pointer to the next node. If the list is empty, it prints an error message
+and does nothing.
 The printList method is the same as before, and prints the values of all nodes in the list.
-The main method creates a new empty list, sets the head to a new node with value 56,
-creates a new node with value 70, and inserts a new node with value 30 after the head,
-and inserts the node with value 70 after the node with value 30. Finally,
-it prints the values of all nodes in the list, which should be 56, 30, and 70.
+The main method creates a new empty list, inserts nodes with values
+56, 30, and 70 at the start of the list, and then calls pop multiple times
+to delete the first node in the list and update the head pointer. Finally,
+it prints the values of all nodes in the list, which should be an empty line after the final call to pop,
+and an error message if pop is called again after the list is empty.
 * */
 public class LinkedList {
     Node head; // Reference to the head of the list
@@ -19,14 +18,19 @@ public class LinkedList {
     }
 
     // Method for inserting a new node with given data after a given node
-    public void insertAfter(Node prevNode, int data) {
-        if (prevNode == null) { // If the given node is null, can't insert after it
-            System.out.println("Previous node cannot be null.");
+    public void insertAtStart(int data) {
+
+        Node newNode = new Node(data); // Create a new node with the given data
+        newNode.next = this.head; // Set the next pointer of the new node to the next node of the previous node
+        this.head = newNode; // Set the next pointer of the previous node to the new node
+    }
+
+    public void pop() {
+        if (this.head == null) { // If the list is empty, there's nothing to delete
+            System.out.println("List is empty.");
             return;
         }
-        Node newNode = new Node(data); // Create a new node with the given data
-        newNode.next = prevNode.next; // Set the next pointer of the new node to the next node of the previous node
-        prevNode.next = newNode; // Set the next pointer of the previous node to the new node
+        this.head = this.head.next; // Update the head pointer to the next node
     }
 
     // Method for printing the values of all nodes in the list
--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,10 +5,16 @@ import org.w3c.dom.*;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList list = new LinkedList(); // Create a new empty list
-        list.head = new Node(56); // Set the head to a new node with value 56
-        Node node2 = new Node(70); // Create a new node with value 70
-        list.insertAfter(list.head, 30); // Insert a new node with value 30 after the head
-        list.insertAfter(list.head.next, 70); // Insert the node with value 70 after the node with value 30
-        list.printList(); // Print the values of all nodes in the list
+        list.insertAtStart(70);
+        list.insertAtStart(30);
+        list.insertAtStart(56);
+        list.printList(); // Should print "56 30 70"
+        list.pop();
+        list.printList(); // Should print "30 70"
+        list.pop();
+        list.printList(); // Should print "70"
+        list.pop();
+        list.printList(); // Should print an empty line
+        list.pop(); // Should print "List is empty."
     }
 }
