1d5fa6dee8de5f9d5a23fc591c1516ffdbdf1961
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-29 15:43:43 +0530

    Added : Insert Between Data In Linked List

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,12 +1,14 @@
 package com.bridgelabz;
-/*The LinkedList class has a new method, append, which appends a new node with the given
- data to the end of the list. If the list is empty,
-it sets the head to the new node. Otherwise, it traverses the list until it reaches the last node,
- and sets the next pointer of the last node to the new node.
+/*The LinkedList class has a new method, insertAfter, which inserts a new node with
+the given data after the given node. If the given node is null, it prints an error
+message and does nothing. Otherwise, it creates a new node with the given data, sets its next
+pointer to the next node of the given node,
+and sets the next pointer of the given node to the new node.
 The printList method is the same as before, and prints the values of all nodes in the list.
-The main method creates a new empty list, and appends three nodes with data values
- 56, 30, and 70 to the end of the list in sequence. Finally, it prints the values of all
-  nodes in the list, which should be 56, 30, and 70.
+The main method creates a new empty list, sets the head to a new node with value 56,
+creates a new node with value 70, and inserts a new node with value 30 after the head,
+and inserts the node with value 70 after the node with value 30. Finally,
+it prints the values of all nodes in the list, which should be 56, 30, and 70.
 * */
 public class LinkedList {
     Node head; // Reference to the head of the list
@@ -16,18 +18,15 @@ public class LinkedList {
         this.head = null; // Initially, the list is empty
     }
 
-    // Method for appending a new node with given data to the end of the list
-    public void append(int data) {
-        Node newNode = new Node(data); // Create a new node with the given data
-        if (this.head == null) { // If the list is empty, set the head to the new node
-            this.head = newNode;
+    // Method for inserting a new node with given data after a given node
+    public void insertAfter(Node prevNode, int data) {
+        if (prevNode == null) { // If the given node is null, can't insert after it
+            System.out.println("Previous node cannot be null.");
             return;
         }
-        Node current = this.head; // Start at the head of the list
-        while (current.next != null) { // Traverse the list until the end
-            current = current.next; // Move to the next node
-        }
-        current.next = newNode; // Set the next pointer of the last node to the new node
+        Node newNode = new Node(data); // Create a new node with the given data
+        newNode.next = prevNode.next; // Set the next pointer of the new node to the next node of the previous node
+        prevNode.next = newNode; // Set the next pointer of the previous node to the new node
     }
 
     // Method for printing the values of all nodes in the list
@@ -40,3 +39,5 @@ public class LinkedList {
         System.out.println(); // Print a newline at the end
     }
 }
+
+
