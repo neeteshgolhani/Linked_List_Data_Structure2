6594d5fe06d17810829931cecb8f12d4de39ce71
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-29 15:42:08 +0530

    Added : Insert Between Data In Linked List

--- src/com/bridgelabz/LinkedListMain.java
+++ src/com/bridgelabz/LinkedListMain.java
@@ -5,9 +5,10 @@ import org.w3c.dom.*;
 public class LinkedListMain {
     public static void main(String[] args) {
         LinkedList list = new LinkedList(); // Create a new empty list
-        list.append(56); // Append the value 56 to the list
-        list.append(30); // Append the value 30 to the list
-        list.append(70); // Append the value 70 to the list
+        list.head = new Node(56); // Set the head to a new node with value 56
+        Node node2 = new Node(70); // Create a new node with value 70
+        list.insertAfter(list.head, 30); // Insert a new node with value 30 after the head
+        list.insertAfter(list.head.next, 70); // Insert the node with value 70 after the node with value 30
         list.printList(); // Print the values of all nodes in the list
     }
 }
